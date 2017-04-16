8ffe8d424f82f34f40f3afd9ab9826347c27056a
edgar.martinez.rico@gmail.com <edgar.martinez.rico@gmail.com>
2017-04-16 11:14:09 -0700

    Smartcab question #3

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -177,7 +177,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=10, tolerance=0.1)
 
 
 if __name__ == '__main__':
